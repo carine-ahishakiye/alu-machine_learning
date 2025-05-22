40bfc8e099b8d32bf440b2f55b5f9969810b7af7
carine-ahishakiye <c.yibukabay@alustudent.com>
2025-05-22 07:42:36 -0700

    3-flip_me_over.py

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module for matrix transpose function.
+"""
+
+def matrix_transpose(matrix):
+    """
+    Return the transpose of a 2D matrix.
+
+    Args:
+        matrix (list of lists): The input 2D matrix.
+
+    Returns:
+        list of lists: The transposed matrix.
+    """
+    rows = len(matrix)
+    cols = len(matrix[0])
+    transposed = []
+
+    for c in range(cols):
+        new_row = []
+        for r in range(rows):
+            new_row.append(matrix[r][c])
+        transposed.append(new_row)
+
+    return transposed
