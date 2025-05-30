7f9ad3784aacbb9f90aac7653d999858b65ac829
carine-ahishakiye <c.yibukabay@alustudent.com>
2025-05-30 10:53:39 -0700

    1-minor.py

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python3
 """
-This module contains a function that calculates
-the minor matrix of a square matrix.
+Function to compute the minor matrix of a given square matrix.
 """
-
 
 def determinant(matrix):
     """
     Recursively calculates the determinant of a matrix.
-
-    Args:
-        matrix (list of lists): The input square matrix.
-
-    Returns:
-        int or float: The determinant of the matrix.
     """
     if matrix == [[]]:
         return 1
@@ -24,20 +16,32 @@ def determinant(matrix):
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
     det = 0
     for col in range(size):
-        sub = [row[:col] + row[col + 1:] for row in matrix[1:]]
-        det += ((-1) ** col) * matrix[0][col] * determinant(sub)
+        minor = [row[:col] + row[col+1:] for row in matrix[1:]]
+        det += ((-1) ** col) * matrix[0][col] * determinant(minor)
     return det
-
 
 def minor(matrix):
     """
-    Calculates the minor matrix of a given square matrix.
+    Calculates the minor matrix of a square matrix.
+    """
+    if (not isinstance(matrix, list)
+            or not all(isinstance(row, list) for row in matrix)):
+        raise TypeError("matrix must be a list of lists")
 
-    Args:
-        matrix (list of lists): A non-empty square matrix.
+    if len(matrix) == 0 or any(len(row) != len(matrix) for row in matrix):
+        raise ValueError("matrix must be a non-empty square matrix")
 
-    Returns:
-        list of lists: The minor matrix.
+    if matrix == [[matrix[0][0]]]:
+        return [[1]]
 
-    Raises:
-        TypeError: If matrix is not a list of lists.
+    size = len(matrix)
+    minors = []
+
+    for i in range(size):
+        row_minors = []
+        for j in range(size):
+            sub = [row[:j] + row[j+1:] for idx, row in enumerate(matrix) if idx != i]
+            row_minors.append(determinant(sub))
+        minors.append(row_minors)
+
+    return minors
