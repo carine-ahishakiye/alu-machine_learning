1f9724a8f413a55f4fe7f19e4b81f1b9af76bfd9
carine-ahishakiye <c.yibukabay@alustudent.com>
2025-05-31 18:18:06 -0700

    1-minor.py

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""This module defines a function to calculate the minor matrix of a square matrix."""
+"""Module to calculate the minor matrix of a square matrix."""
 
 
 def minor(matrix):
