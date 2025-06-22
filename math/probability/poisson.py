7695c925e0d67115d77d3585969b9f2f3baf321c
carine-ahishakiye <c.yibukabay@alustudent.com>
2025-06-21 20:30:51 -0700

    poisson.py

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -6,12 +6,7 @@ class Poisson:
     """Represents a Poisson distribution"""
 
     def __init__(self, data=None, lambtha=1.):
-        """Class constructor
-
-        Args:
-            data (list): List of data points to estimate lambtha.
-            lambtha (float): Expected number of occurrences in a given time frame.
-        """
+        """Initialize a Poisson distribution"""
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
@@ -20,6 +15,7 @@ class Poisson:
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
             if len(data) < 2:
-                raise ValueError("data must contain multiple values")
-            # Estimate lambtha as the mean of the data
+                raise ValueError(
+                    "data must contain multiple values"
+                )
             self.lambtha = float(sum(data) / len(data))
