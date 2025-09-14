c853b1d775a57a2c7ebdc0433f1d678f8583dc83
carine-ahishakiye <c.yibukabay@alustudent.com>
2025-09-14 10:49:09 -0700

    9-neural_network.py

---
+++ supervised_learning/classification/9-neural_network.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""
+NeuralNetwork class with one hidden layer performing
+binary classification, private attributes with getters
+"""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """Neural network with one hidden layer"""
+
+    def __init__(self, nx, nodes):
+        """
+        Initialize the neural network
+        Args:
+            nx (int): number of input features
+            nodes (int): number of nodes in the hidden layer
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        # Hidden layer private attributes
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+
+        # Output neuron private attributes
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    # Getters for private attributes
+    @property
+    def W1(self):
+        """Getter for hidden layer weights"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Getter for hidden layer bias"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Getter for hidden layer activation"""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Getter for output neuron weights"""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Getter for output neuron bias"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Getter for output neuron activation"""
+        return self.__A2
