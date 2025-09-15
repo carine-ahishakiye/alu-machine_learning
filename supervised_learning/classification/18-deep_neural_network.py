f11f25f3595978ebee9d01945aadaaf7eba25872
carine-ahishakiye <c.yibukabay@alustudent.com>
2025-09-15 13:01:28 -0700

    18-deep_neural_network.py

--- supervised_learning/classification/18-deep_neural_network.py
+++ supervised_learning/classification/18-deep_neural_network.py
@@ -1,35 +1,34 @@
 #!/usr/bin/env python3
-"""18-deep_neural_network.py"""
-
+"""DeepNeuralNetwork performing binary classification"""
 import numpy as np
 
 
 class DeepNeuralNetwork:
-    """Defines a deep neural network performing binary classification."""
+    """Defines a deep neural network performing binary classification"""
 
     def __init__(self, nx, layers):
-        # Validate nx
+        """Class constructor"""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-
-        # Validate layers type and not empty
         if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        if not all(isinstance(l, int) and l > 0 for l in layers):
             raise TypeError("layers must be a list of positive integers")
 
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = {}
 
-        prev_nodes = nx
-        # Single loop: validate nodes + initialize weights/biases
-        for i, nodes in enumerate(layers):
-            if not isinstance(nodes, int) or nodes <= 0:
-                raise TypeError("layers must be a list of positive integers")
-            self.__weights["W" + str(i + 1)] = np.random.randn(nodes, prev_nodes) * np.sqrt(2 / prev_nodes)
-            self.__weights["b" + str(i + 1)] = np.zeros((nodes, 1))
-            prev_nodes = nodes
+        # Initialize weights using He et al. method, biases to 0
+        layers_dims = [nx] + layers
+        for l in range(1, self.__L + 1):
+            self.__weights["W" + str(l)] = (np.random.randn(
+                layers_dims[l], layers_dims[l - 1])
+                * np.sqrt(2 / layers_dims[l - 1])
+            )
+            self.__weights["b" + str(l)] = np.zeros((layers_dims[l], 1))
 
     @property
     def L(self):
@@ -56,7 +55,8 @@ class DeepNeuralNetwork:
             Wl = self.__weights["W" + str(l)]
             bl = self.__weights["b" + str(l)]
             Zl = np.dot(Wl, A_prev) + bl
-            A_prev = 1 / (1 + np.exp(-Zl))  # Sigmoid activation
+            # Sigmoid activation
+            A_prev = 1 / (1 + np.exp(-Zl))
             self.__cache["A" + str(l)] = A_prev
 
         return A_prev, self.__cache
