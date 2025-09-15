83a309cea9260fe8afffb9851fe1c927877c209c
carine-ahishakiye <c.yibukabay@alustudent.com>
2025-09-15 13:12:50 -0700

    18-deep_neural_network.py

--- supervised_learning/classification/18-deep_neural_network.py
+++ supervised_learning/classification/18-deep_neural_network.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""DeepNeuralNetwork performing binary classification"""
+"""Deep Neural Network for binary classification"""
 import numpy as np
 
 
@@ -14,47 +14,45 @@ class DeepNeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(l, int) and l > 0 for l in layers):
+        if not all(isinstance(n, int) and n > 0 for n in layers):
             raise TypeError("layers must be a list of positive integers")
 
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = {}
 
-        prev_nodes = nx
-        for i, nodes in enumerate(layers, 1):
-            self.__weights["W" + str(i)] = (
-                np.random.randn(nodes, prev_nodes) * np.sqrt(2 / prev_nodes)
-            )
-            self.__weights["b" + str(i)] = np.zeros((nodes, 1))
-            prev_nodes = nodes
+        for l in range(self.__L):
+            layer_size = layers[l]
+            if l == 0:
+                prev_size = nx
+            else:
+                prev_size = layers[l-1]
+            # He et al. initialization
+            self.__weights['W' + str(l + 1)] = np.random.randn(layer_size, prev_size) * np.sqrt(2 / prev_size)
+            self.__weights['b' + str(l + 1)] = np.zeros((layer_size, 1))
 
     @property
     def L(self):
-        """Getter for number of layers"""
+        """Number of layers"""
         return self.__L
 
     @property
     def cache(self):
-        """Getter for cache dictionary"""
+        """Intermediate values"""
         return self.__cache
 
     @property
     def weights(self):
-        """Getter for weights dictionary"""
+        """Weights and biases"""
         return self.__weights
 
     def forward_prop(self, X):
         """Calculates forward propagation of the neural network"""
-        self.__cache["A0"] = X
-        A = X
-
-        # Only one loop over layers
-        for l in range(1, self.__L + 1):
-            W = self.__weights["W" + str(l)]
-            b = self.__weights["b" + str(l)]
-            Z = np.dot(W, A) + b
-            A = 1 / (1 + np.exp(-Z))  # Sigmoid activation
-            self.__cache["A" + str(l)] = A
-
-        return A, self.__cache
+        self.__cache['A0'] = X
+        for l in range(self.__L):
+            W = self.__weights['W' + str(l + 1)]
+            b = self.__weights['b' + str(l + 1)]
+            A_prev = self.__cache['A' + str(l)]
+            Z = np.dot(W, A_prev) + b
+            self.__cache['A' + str(l + 1)] = 1 / (1 + np.exp(-Z))  # Sigmoid
+        return self.__cache['A' + str(self.__L)], self.__cache
