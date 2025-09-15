6d986e66ea707307aca438dce713513b3332b31b
carine-ahishakiye <c.yibukabay@alustudent.com>
2025-09-15 13:11:12 -0700

    18-deep_neural_network.py

--- supervised_learning/classification/18-deep_neural_network.py
+++ supervised_learning/classification/18-deep_neural_network.py
@@ -21,14 +21,13 @@ class DeepNeuralNetwork:
         self.__cache = {}
         self.__weights = {}
 
-        # Initialize weights using He et al. method, biases to 0
-        layers_dims = [nx] + layers
-        for l in range(1, self.__L + 1):
-            self.__weights["W" + str(l)] = (np.random.randn(
-                layers_dims[l], layers_dims[l - 1])
-                * np.sqrt(2 / layers_dims[l - 1])
+        prev_nodes = nx
+        for i, nodes in enumerate(layers, 1):
+            self.__weights["W" + str(i)] = (
+                np.random.randn(nodes, prev_nodes) * np.sqrt(2 / prev_nodes)
             )
-            self.__weights["b" + str(l)] = np.zeros((layers_dims[l], 1))
+            self.__weights["b" + str(i)] = np.zeros((nodes, 1))
+            prev_nodes = nodes
 
     @property
     def L(self):
@@ -48,15 +47,14 @@ class DeepNeuralNetwork:
     def forward_prop(self, X):
         """Calculates forward propagation of the neural network"""
         self.__cache["A0"] = X
-        A_prev = X
+        A = X
 
         # Only one loop over layers
         for l in range(1, self.__L + 1):
-            Wl = self.__weights["W" + str(l)]
-            bl = self.__weights["b" + str(l)]
-            Zl = np.dot(Wl, A_prev) + bl
-            # Sigmoid activation
-            A_prev = 1 / (1 + np.exp(-Zl))
-            self.__cache["A" + str(l)] = A_prev
+            W = self.__weights["W" + str(l)]
+            b = self.__weights["b" + str(l)]
+            Z = np.dot(W, A) + b
+            A = 1 / (1 + np.exp(-Z))  # Sigmoid activation
+            self.__cache["A" + str(l)] = A
 
-        return A_prev, self.__cache
+        return A, self.__cache
