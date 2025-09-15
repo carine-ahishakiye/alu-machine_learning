2574b99db8110a292f3cd9e6a02e1f0bc639e538
carine-ahishakiye <c.yibukabay@alustudent.com>
2025-09-14 17:40:44 -0700

    16-deep_neural_network.py

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
-"""Deep Neural Network performing binary classification"""
-
+"""
+Defines a deep neural network performing binary classification
+"""
 
 import numpy as np
 
 
 class DeepNeuralNetwork:
-    """Defines a deep neural network for binary classification"""
+    """
+    Defines a deep neural network for binary classification
+    """
 
     def __init__(self, nx, layers):
         """
-        Class constructor
-
-        Args:
-            nx (int): number of input features
-            layers (list): list representing the number of nodes in each layer
+        Initialize the deep neural network
+        nx: number of input features
+        layers: list representing the number of nodes in each layer
         """
         # Validate nx
         if not isinstance(nx, int):
@@ -25,22 +26,24 @@ class DeepNeuralNetwork:
         # Validate layers
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(x, int) and x > 0 for x in layers):
+        if not all(isinstance(l, int) and l > 0 for l in layers):
             raise TypeError("layers must be a list of positive integers")
 
-        self.L = len(layers)       # number of layers
-        self.cache = {}            # store intermediate values
-        self.weights = {}          # store weights and biases
+        self.L = len(layers)        # number of layers
+        self.cache = {}             # store intermediary values
+        self.weights = {}           # store weights and biases
 
-        # Initialize weights and biases using He et al. method
-        for l in range(1, self.L + 1):
-            if l == 1:
-                prev_nodes = nx
-            else:
-                prev_nodes = layers[l - 2]
+        # Single loop to initialize weights and biases
+        for i in range(self.L):
+            # layer sizes
+            layer_size = layers[i]
+            prev_size = nx if i == 0 else layers[i - 1]
 
-            self.weights["W" + str(l)] = (
-                np.random.randn(layers[l - 1], prev_nodes)
-                * np.sqrt(2 / prev_nodes)
+            # He initialization for weights
+            self.weights['W' + str(i + 1)] = (
+                np.random.randn(layer_size, prev_size) *
+                np.sqrt(2 / prev_size)
             )
-            self.weights["b" + str(l)] = np.zeros((layers[l - 1], 1))
+
+            # Zero initialization for biases
+            self.weights['b' + str(i + 1)] = np.zeros((layer_size, 1))
