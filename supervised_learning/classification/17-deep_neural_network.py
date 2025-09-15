26320f0a22b58de144313154c3d1ec3f1e897dbe
carine-ahishakiye <c.yibukabay@alustudent.com>
2025-09-15 12:57:47 -0700

    17-deep_neural_network.py

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -1,22 +1,13 @@
 #!/usr/bin/env python3
-"""
-Defines a deep neural network performing binary classification with private attributes
-"""
+"""17-deep_neural_network.py"""
 
 import numpy as np
 
 
 class DeepNeuralNetwork:
-    """Deep neural network performing binary classification"""
+    """Defines a deep neural network performing binary classification."""
 
     def __init__(self, nx, layers):
-        """
-        Class constructor
-
-        Args:
-            nx (int): number of input features
-            layers (list): list representing the number of nodes in each layer
-        """
         # Validate nx
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
@@ -26,39 +17,33 @@ class DeepNeuralNetwork:
         # Validate layers
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(x, int) and x > 0 for x in layers):
-            raise TypeError("layers must be a list of positive integers")
 
-        self.__L = len(layers)      # number of layers
-        self.__cache = {}           # dictionary to hold intermediary values
-        self.__weights = {}         # dictionary to hold weights and biases
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
 
-        # Initialize weights and biases using one loop
-        for l in range(self.__L):
-            layer_size = layers[l]
-            prev_size = nx if l == 0 else layers[l - 1]
+        prev_nodes = nx
+        # Single loop for validation and weight initialization
+        for i, nodes in enumerate(layers):
+            if not isinstance(nodes, int) or nodes <= 0:
+                raise TypeError("layers must be a list of positive integers")
+            # He et al. initialization for weights
+            self.__weights["W" + str(i + 1)] = np.random.randn(nodes, prev_nodes) * np.sqrt(2 / prev_nodes)
+            # Bias initialization to zeros
+            self.__weights["b" + str(i + 1)] = np.zeros((nodes, 1))
+            prev_nodes = nodes
 
-            # He initialization for weights
-            self.__weights['W' + str(l + 1)] = (
-                np.random.randn(layer_size, prev_size) *
-                np.sqrt(2 / prev_size)
-            )
-
-            # Bias initialized to zeros
-            self.__weights['b' + str(l + 1)] = np.zeros((layer_size, 1))
-
-    # Getter properties
     @property
     def L(self):
-        """Number of layers"""
+        """Getter for number of layers"""
         return self.__L
 
     @property
     def cache(self):
-        """Dictionary to hold intermediary values"""
+        """Getter for cache dictionary"""
         return self.__cache
 
     @property
     def weights(self):
-        """Dictionary to hold weights and biases"""
+        """Getter for weights dictionary"""
         return self.__weights
