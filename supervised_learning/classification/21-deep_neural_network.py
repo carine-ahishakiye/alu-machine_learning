017d192ed5eeb89fb0f4e4fa4455df5ae0b4702d
carine-ahishakiye <c.yibukabay@alustudent.com>
2025-09-15 17:42:03 -0700

    21-deep_neural_network.py

--- supervised_learning/classification/21-deep_neural_network.py
+++ supervised_learning/classification/21-deep_neural_network.py
@@ -27,20 +27,23 @@ class DeepNeuralNetwork:
         self.__cache = {}
         self.__weights = {}
 
-        for l in range(self.__L):
-            if not isinstance(layers[l], int) or layers[l] <= 0:
+        for layer_idx in range(self.__L):
+            if not isinstance(layers[layer_idx], int) or layers[layer_idx] <= 0:
                 raise TypeError("layers must be a list of positive integers")
 
-            if l == 0:
+            if layer_idx == 0:
                 he_init = np.sqrt(2 / nx)
                 self.__weights['W1'] = np.random.randn(
                     layers[0], nx) * he_init
             else:
-                he_init = np.sqrt(2 / layers[l - 1])
-                self.__weights['W' + str(l + 1)] = (
-                    np.random.randn(layers[l], layers[l - 1]) * he_init
+                he_init = np.sqrt(2 / layers[layer_idx - 1])
+                self.__weights['W' + str(layer_idx + 1)] = (
+                    np.random.randn(layers[layer_idx],
+                                    layers[layer_idx - 1]) * he_init
                 )
-            self.__weights['b' + str(l + 1)] = np.zeros((layers[l], 1))
+            self.__weights['b' + str(layer_idx + 1)] = np.zeros(
+                (layers[layer_idx], 1)
+            )
 
     @property
     def L(self):
@@ -61,15 +64,15 @@ class DeepNeuralNetwork:
         """Performs forward propagation"""
         self.__cache['A0'] = X
 
-        for l in range(1, self.__L + 1):
-            Wl = self.__weights['W' + str(l)]
-            bl = self.__weights['b' + str(l)]
-            A_prev = self.__cache['A' + str(l - 1)]
-            Zl = np.matmul(Wl, A_prev) + bl
-            Al = 1 / (1 + np.exp(-Zl))
-            self.__cache['A' + str(l)] = Al
+        for layer_idx in range(1, self.__L + 1):
+            W = self.__weights['W' + str(layer_idx)]
+            b = self.__weights['b' + str(layer_idx)]
+            A_prev = self.__cache['A' + str(layer_idx - 1)]
+            Z = np.matmul(W, A_prev) + b
+            A = 1 / (1 + np.exp(-Z))
+            self.__cache['A' + str(layer_idx)] = A
 
-        return Al, self.__cache
+        return A, self.__cache
 
     def cost(self, Y, A):
         """Calculates cost using logistic regression"""
@@ -94,23 +97,23 @@ class DeepNeuralNetwork:
         alpha: learning rate
         """
         m = Y.shape[1]
-        weights = self.__weights.copy()
+        weights_copy = self.__weights.copy()
         dZ = cache['A' + str(self.__L)] - Y  # output layer error
 
-        for l in reversed(range(1, self.__L + 1)):
-            A_prev = cache['A' + str(l - 1)]
-            Wl = weights['W' + str(l)]
+        for layer_idx in reversed(range(1, self.__L + 1)):
+            A_prev = cache['A' + str(layer_idx - 1)]
+            W = weights_copy['W' + str(layer_idx)]
 
             # Gradients
             dW = (1 / m) * np.matmul(dZ, A_prev.T)
             db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
 
             # Update weights
-            self.__weights['W' + str(l)] = Wl - alpha * dW
-            self.__weights['b' + str(l)] = (
-                self.__weights['b' + str(l)] - alpha * db
+            self.__weights['W' + str(layer_idx)] = W - alpha * dW
+            self.__weights['b' + str(layer_idx)] = (
+                self.__weights['b' + str(layer_idx)] - alpha * db
             )
 
-            if l > 1:
-                A_prev = cache['A' + str(l - 1)]
-                dZ = np.matmul(Wl.T, dZ) * (A_prev * (1 - A_prev))
+            if layer_idx > 1:
+                A_prev = cache['A' + str(layer_idx - 1)]
+                dZ = np.matmul(W.T, dZ) * (A_prev * (1 - A_prev))
