14b34233d8eb282c6e7af64cfd27de6671914299
carine-ahishakiye <c.yibukabay@alustudent.com>
2025-09-21 17:58:36 -0700

    train

--- supervised_learning/tensorflow/5-create_train_op.py
+++ supervised_learning/tensorflow/5-create_train_op.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """
-This module contains a function to create the training operation
-for a neural network using gradient descent.
+This module contains a function to create the training operation for a neural network.
 """
 
 import tensorflow as tf
@@ -9,15 +8,14 @@ import tensorflow as tf
 
 def create_train_op(loss, alpha):
     """
-    Creates a training operation that minimizes the loss
-    using gradient descent.
+    Creates the training operation for the network using gradient descent.
 
     Args:
-        loss: tensor containing the loss of the network
+        loss: tensor containing the loss of the network’s prediction
         alpha: learning rate
 
     Returns:
-        TensorFlow operation to train the network
+        TensorFlow operation that trains the network
     """
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=alpha)
     train_op = optimizer.minimize(loss)
