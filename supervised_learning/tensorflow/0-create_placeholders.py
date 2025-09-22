7f751010090d58474ee43485c3feee9ebbb22106
carine-ahishakiye <c.yibukabay@alustudent.com>
2025-09-21 17:31:22 -0700

    create_placeholders.py

---
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+
+def create_placeholders(nx, classes):
+    """
+    Creates placeholders for a neural network.
+
+    Args:
+        nx (int): Number of input features
+        classes (int): Number of classes for classification
+
+    Returns:
+        x: placeholder for input data
+        y: placeholder for one-hot labels
+    """
+    x = tf.placeholder(tf.float32, shape=(None, nx), name='x')
+    y = tf.placeholder(tf.float32, shape=(None, classes), name='y')
+    return x, y
