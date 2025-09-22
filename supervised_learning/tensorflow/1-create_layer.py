4186ad6c172963d10c81d4acaac03f579c33947a
carine-ahishakiye <c.yibukabay@alustudent.com>
2025-09-21 17:37:33 -0700

    create_layer

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -8,7 +8,17 @@ import tensorflow as tf
 
 
 def create_layer(prev, n, activation):
+    """
+    Creates a fully connected layer in a neural network.
 
+    Args:
+        prev: tensor output from the previous layer
+        n (int): number of nodes in this layer
+        activation: activation function for the layer
+
+    Returns:
+        The tensor output of the layer
+    """
     he_init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     layer = tf.layers.Dense(
         units=n,
