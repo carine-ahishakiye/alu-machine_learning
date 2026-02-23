9613e21a62b6423cb2493c8cce634e2503f4a8ab
carine-ahishakiye <c.yibukabay@alustudent.com>
2026-02-23 00:52:03 -0800

    Add PCA with variance threshold

---
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""PCA on a dataset"""
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """Performs PCA on a dataset.
+
+    Args:
+        X: numpy.ndarray of shape (n, d) containing the dataset
+        var: fraction of variance to maintain
+
+    Returns:
+        W: numpy.ndarray of shape (d, nd) containing the weights matrix
+    """
+    _, s, Vt = np.linalg.svd(X)
+    cumvar = np.cumsum(s ** 2) / np.sum(s ** 2)
+    nd = np.argmax(cumvar >= var) + 1
+    W = Vt[:nd].T
+
+    return W
