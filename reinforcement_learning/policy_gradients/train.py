1aec307f4137bb181480d4b094359e6195a5adae
carine-ahishakiye <c.yibukabay@alustudent.com>
2026-03-14 01:56:51 -0700

    Add show_result parameter to render every 1000 episodes

--- reinforcement_learning/policy_gradients/train.py
+++ reinforcement_learning/policy_gradients/train.py
@@ -6,7 +6,7 @@ import numpy as np
 from policy_gradient import policy_gradient
 
 
-def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
+def train(env, nb_episodes, alpha=0.000045, gamma=0.98, show_result=False):
     weight = np.random.rand(
         env.observation_space.shape[0],
         env.action_space.n
@@ -23,6 +23,9 @@ def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
         done = False
 
         while not done:
+            if show_result and (episode % 1000 == 0):
+                env.render()
+
             action, gradient = policy_gradient(state, weight)
             next_state, reward, done, _ = env.step(action)
             next_state = next_state[None, :]
